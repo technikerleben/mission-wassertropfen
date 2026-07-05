b40e4b034039c6026d13d0f388dec7a55471d161
technikerleben <christophvillis@googlemail.com>
2026-07-05 14:43:52 +0200

    Ergänze Glossar für die Lernzusammenfassung

---
+++ src/content/glossary.js
@@ -0,0 +1,38 @@
+export const glossary = [
+  {
+    term: 'Wassermolekül (H₂O)',
+    definition: 'Das kleinste Teilchen von Wasser. Es besteht aus zwei Wasserstoffatomen und einem Sauerstoffatom.'
+  },
+  {
+    term: 'Aggregatzustand',
+    definition: 'Die Form, in der ein Stoff vorkommt. Wasser kann fest, flüssig oder gasförmig sein.'
+  },
+  {
+    term: 'Verdunstung',
+    definition: 'Flüssiges Wasser wird gasförmig. Einzelne Wassermoleküle verlassen dabei die Wasseroberfläche.'
+  },
+  {
+    term: 'Wasserdampf',
+    definition: 'Gasförmiges Wasser in der Luft. Wasserdampf ist unsichtbar.'
+  },
+  {
+    term: 'Kondensation',
+    definition: 'Gasförmiger Wasserdampf wird wieder flüssig. So entstehen winzige Wassertröpfchen und Wolken.'
+  },
+  {
+    term: 'Niederschlag',
+    definition: 'Wasser fällt aus Wolken zur Erde, zum Beispiel als Regen, Schnee oder Hagel.'
+  },
+  {
+    term: 'Versickerung',
+    definition: 'Wasser dringt durch kleine Zwischenräume in den Boden ein und bewegt sich langsam nach unten.'
+  },
+  {
+    term: 'Grundwasser',
+    definition: 'Wasser, das sich unter der Erdoberfläche in Hohlräumen und Spalten von Boden und Gestein befindet.'
+  },
+  {
+    term: 'Wasserkreislauf',
+    definition: 'Die fortlaufende Bewegung des Wassers zwischen Erdoberfläche, Boden und Atmosphäre.'
+  }
+];
